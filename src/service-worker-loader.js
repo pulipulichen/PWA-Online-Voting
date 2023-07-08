af1bbf40dc7a1989430ab5e1e916d7b74252573e
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-07-09 00:34:45 +0800

    Sun Jul  9 12:34:45 AM CST 2023

--- src/service-worker-loader.js
+++ src/service-worker-loader.js
@@ -7,19 +7,19 @@ if (enableServiceWorker) {  // 不使用service-worker快取，這樣會無法�
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
 
-      // This variable will save the event for later use.
-      let deferredPrompt;
-      window.addEventListener('beforeinstallprompt', (e) => {
-        // Prevents the default mini-infobar or install dialog from appearing on mobile
-        e.preventDefault();
-        // Save the event because you'll need to trigger it later.
-        deferredPrompt = e;
-        // Show your customized install prompt for your PWA
-        // Your own UI doesn't have to be a single element, you
-        // can have buttons in different locations, or wait to prompt
-        // as part of a critical journey.
-        showInAppInstallPromotion();
-      });
+      // // This variable will save the event for later use.
+      // let deferredPrompt;
+      // window.addEventListener('beforeinstallprompt', (e) => {
+      //   // Prevents the default mini-infobar or install dialog from appearing on mobile
+      //   e.preventDefault();
+      //   // Save the event because you'll need to trigger it later.
+      //   deferredPrompt = e;
+      //   // Show your customized install prompt for your PWA
+      //   // Your own UI doesn't have to be a single element, you
+      //   // can have buttons in different locations, or wait to prompt
+      //   // as part of a critical journey.
+      //   showInAppInstallPromotion();
+      // });
     }, function (err) {
 
       // registration failed
